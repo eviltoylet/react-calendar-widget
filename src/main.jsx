46bea4659f3e0a24e1661a145ec87df64d0db032
Brian Chang <bchang85@hotmail.com>
2015-11-22 17:28:48 -0800

    Add logic to determine and position days of the month

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,12 @@ var Table = require('./table/');
 var CalendarWidget = React.createClass({
     getInitialState: function () {
         var date = new Date();
+        var day = date.getDate();
         var month = date.getMonth();
         var year = date.getFullYear();
         return {
             date: date,
+            day: day,
             month: month,
             year: year
         }
@@ -19,7 +21,7 @@ var CalendarWidget = React.createClass({
         return (
             <div>
                 <Header year={this.state.year} month={this.state.month}/>
-                <Table/>
+                <Table year={this.state.year} month={this.state.month} day={this.state.day}/>
             </div>
         );
     }
--- src/table/index.jsx
+++ src/table/index.jsx
@@ -1,15 +1,68 @@
 var React = require('react');
 
-// TODO: Implement the logic to determine what day of the week the first day of the month starts on
+// https://cs.uwaterloo.ca/~alopez-o/math-faq/node73.html
+var dayOfWeekForFirstDateInMonth = function (month, year) {
+    var date = new Date(year, month, 1);
+    return date.getDay();
+};
+
+var isLeapYear = function (year) {
+    if (year % 4 == 0) {
+        if (year % 100) {
+            if (year % 400) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    return false;
+};
+
+var daysInMonth = function (month, year) {
+    if (month == 1) {
+        var leapYear = isLeapYear(year);
+        return leapYear ? 29 : 28;
+    } else {
+        return month % 2 == 0 ? 31 : 30;
+    }
+};
+
+// TODO: Refactor this code to make it cleaner
 var Table = React.createClass({
     render: function () {
-        var day = 0;
+        var startingDayOfWeek = dayOfWeekForFirstDateInMonth(this.props.month, this.props.year);
+        var day = 1;
+        var start = false;
+        var stop = false;
         var tableRows = [];
+        var numberOfDays = daysInMonth(this.props.month, this.props.year);
+        tableRows.push(
+            <tr>
+                <td>S</td>
+                <td>M</td>
+                <td>T</td>
+                <td>W</td>
+                <td>T</td>
+                <td>F</td>
+                <td>S</td>
+            </tr>);
         for (var x = 0; x < 5; x++) {
             var tableCols = [];
             for (var y = 0; y < 7; y++) {
-                tableCols.push(<td key={"col_" + y}>{day}</td>);
-                day++;
+                if (startingDayOfWeek == y) {
+                    start = true;
+                }
+                tableCols.push(<td key={"col_" + y}>{ start  && !stop ? day : ""}</td>);
+                if (start) {
+                    day++;
+                }
+                if (day > numberOfDays) {
+                    stop = true;
+                }
             }
             tableRows.push(<tr key={"row_" + x}>{tableCols}</tr>);
         }

8e25e9f30b7957dd2a49f5b9ba018c10b7ab28ba
Brian Chang <bchang85@hotmail.com>
2015-11-27 18:50:41 -0800

    Replace 'that' with 'self'

--- src/main.jsx
+++ src/main.jsx
@@ -21,15 +21,12 @@ var CalendarWidget = React.createClass({
             date: date
         }
     },
-    componentDidMount: function () {
-        this.state.callbacks.onDaySelect(this.state.today);
-    },
     render: function () {
         var self = this;
         var updateDate = function (year, month, day) {
-            var existingYear = that.state.date.getFullYear();
-            var existingMonth = that.state.date.getMonth();
-            var existingDay = that.state.date.getDate();
+            var existingYear = self.state.date.getFullYear();
+            var existingMonth = self.state.date.getMonth();
+            var existingDay = self.state.date.getDate();
 
             var date = new Date(year == null ? existingYear : year, month == null ? existingMonth : month, day == null ? existingDay : day);
             self.setState({
